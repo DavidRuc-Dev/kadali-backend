1665dea4ce8f9081d217d8ef817e3a7b3e1e517d
DavidRuc-Dev <oplogicontacto@gmail.com>
2025-06-30 15:13:59 -0500

    feat: :sparkles: new features to the backend

--- src/index.ts
+++ src/index.ts
@@ -8,26 +8,42 @@ const notion = new Client({ auth: process.env.NOTION_TOKEN });
 const app = express();
 app.use(express.json());
 
-app.post('/api/mensajes', async (req, res): Promise<any> => {
-  const { nombre, mensaje } = req.body;
+app.get('/', (_, res) => {
+  res.send('Servidor funcionando 🚀');
+});
 
-  if (!nombre || !mensaje) {
+app.post('/api/mensajes', async (req, res): Promise<any> => {
+  const { persona, correo, numero, plan } = req.body;
+
+  if (!persona || !correo) {
     return res.status(400).json({ error: 'Nombre y mensaje son requeridos' });
   }
 
   try {
+       console.log("Payload enviado a Notion:", JSON.stringify({
+    Nombre: { title: [{ text: { content: persona } }] },
+    Mensaje: { rich_text: [{ text: { content: plan } }] },
+    Numero: { phone_number: numero },
+    Correo: { email: correo },
+}, null, 2));
     await notion.pages.create({
       parent: { database_id: process.env.NOTION_DB_ID! },
       properties: {
-        Nombre: {
-          title: [{ text: { content: nombre } }],
+        Persona: {
+          title: [{ text: { content: persona } }],
         },
-        Mensaje: {
-          rich_text: [{ text: { content: mensaje } }],
+        Correo: {
+          email: correo,
+        },
+        Numero: {
+          rich_text: [{ text: { content: numero } }],
+        },
+        Plan: {
+          rich_text: [{ text: { content: plan } }],
         },
       },
     });
-
+ 
     res.status(200).json({ success: true });
   } catch (error) {
     console.error('Error al guardar en Notion:', error);
